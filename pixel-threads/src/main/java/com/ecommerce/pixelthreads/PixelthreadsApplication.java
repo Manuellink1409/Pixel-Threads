67d7637157de72a25ea1be44dd503383e56a785c
Manuellink1409 <manuelaft14@gmail.com>
2024-07-07 17:49:25 -0600

    first commit

---
+++ pixel-threads/src/main/java/com/ecommerce/pixelthreads/PixelthreadsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.pixelthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelthreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelthreadsApplication.class, args);
+	}
+
+}
---
+++ pixel-threads/src/test/java/com/ecommerce/pixelthreads/PixelthreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.pixelthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelthreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
